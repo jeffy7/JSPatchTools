8c19741c90b5a1236246caf6eb1fb2fabc48fa2f
Cwwng <Cwwng_wu@talkmate.com>
2016-05-27 11:49:13 +0800

    Initial Commit

---
+++ Tools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tools
+//
+//  Created by cwwng on 16/5/27.
+//  Copyright © 2016年 cwwng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tools/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Tools
+//
+//  Created by cwwng on 16/5/27.
+//  Copyright © 2016年 cwwng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
